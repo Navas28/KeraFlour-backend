bcdcc57f6da906d21b58165e70ff2ca1fddfcc1e
Navas . M <navasmhd79@gmail.com>
2025-08-31 11:43:03 +0530

    completed auth

--- model/User.js
+++ model/User.js
@@ -3,7 +3,8 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
     uid: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
+    name: {type: String},
     role: { type: String, enum: ["user", "admin"], default: "user" },
 });
 
-export default mongoose.model.User || mongoose.model("User", userSchema);
+export default mongoose.models.User || mongoose.model("User", userSchema);
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,23 +2,26 @@ import express from "express";
 import admin from "../config/firebase.js";
 import User from "../model/User.js";
 
-const router = express.Router()
+const router = express.Router();
 
 router.post("/sync-user", async (req, res) => {
-    const {token} = req.body;
+    const { token, name } = req.body;
 
     try {
-        const decodedToken = await admin.auth().verifyIdToken(token)
-        const {uid, email} = decodedToken;
+        const decodedToken = await admin.auth().verifyIdToken(token);
+        const { uid, email } = decodedToken;
 
-        let user = await User.findOne({uid})
-        if(!user){
-            user = new User({uid, email, role: "user"})
-            await user.save()
+        let user = await User.findOne({ uid });
+        if (!user) {
+            user = new User({ uid, email, name, role: "user" });
+        } else if (name && !user.name) {
+            user.name = name;
         }
-        res.json({uid, email, role: user.role})
+        await user.save();
+
+        res.json({ uid, email, name: user.name, role: user.role });
     } catch (error) {
-        res.status(401).json({message: "Invalid token"})
+        res.status(401).json({ message: "Invalid token" });
     }
-})
+});
 export default router;
